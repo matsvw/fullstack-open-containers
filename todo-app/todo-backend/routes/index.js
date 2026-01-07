6c9977ad5c7d672c73869ea5dfe15efa3ab4bd77
Mats <mats.weissenberg@gmail.com>
2026-01-07 12:47:33 +0200

    Exercise 12.9

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const configs = require('../util/config')
+const redis = require('../redis')
 
 let visits = 0
 
@@ -14,5 +15,7 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+
 
 module.exports = router;
